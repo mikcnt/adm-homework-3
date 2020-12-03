94a3b4959cbee07917979e3f45c1bd87238e8593
Michele Conti <11929535+mikcnt@users.noreply.github.com>
2020-12-03 14:00:23 +0100

    Updated files, directories and stuff to deliver hw

--- functions.py
+++ functions.py
@@ -25,11 +25,11 @@ def book_title_summary(df):
     return df[(df['bookTitle'].isna())].head()
 
 def save_obj(obj, name):
-    with open(name + '.pkl', 'wb') as f:
+    with open('./indexes/' + name + '.pkl', 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
 def load_obj(name):
-    with open(name + '.pkl', 'rb') as f:
+    with open('./indexes/' + name + '.pkl', 'rb') as f:
         return pickle.load(f)
     
 def rating_value_summary(df):
