30400cc7a1b7d594f8502feed15f25dba993beca
Michele Conti <11929535+mikcnt@users.noreply.github.com>
2020-12-03 22:03:50 +0100

    Docstrings for every function in functions.py

--- functions.py
+++ functions.py
@@ -19,6 +19,14 @@ import matplotlib.pyplot as plt
 
 # Utils
 def book_title_summary(df):
+    """Show a summary of the missing values for the title column of the dataframe.
+
+    Args:
+        df (pd.DataFrame): Input dataframe.
+
+    Returns:
+        pd.DataFrame: Rows of the dataframe for which the title values are missing.
+    """
     n_missing = df[(df['bookTitle'].isna())].shape[0]
     print('There are {} instances that are missing the `bookTitle` column.'.format(n_missing))
     print()
@@ -33,6 +41,11 @@ def load_obj(name):
         return pickle.load(f)
     
 def rating_value_summary(df):
+    """Print stats of the values of the rating value column.
+
+    Args:
+        df (pd.DataFrame): Input dataframe.
+    """
     print('There are {} books with rating value between 0 and 4'.format(df[(df['ratingValue'] > 0) & (df['ratingValue'] < 4)].shape[0]))
     print('There are {} books with rating value greater than 4'.format(df[(df['ratingValue'] > 4)].shape[0]))
 
@@ -41,12 +54,33 @@ def rating_value_summary(df):
 # Preprocessing
 
 def remove_punctuation(s):
+    """Remove punctuation from a string. Notice that this works in two different ways:
+    Either the punctuation is simply removed from the string (meaning it is just replaced with an empty string),
+    or it is replaced with a space. This happens when the punctuation is in the middle of a string.
+    
+    >>> remove_punctuation('game-changing, life saver!')
+    'game changing life saver'
+
+    Args:
+        s (str): Input string.
+
+    Returns:
+        str: String without punctuation.
+    """
     reg1 = re.compile(r'\b[^\w\s]\b')
     reg2 = re.compile(r'[^\w\s]')
     first_pass = reg1.sub(' ', s)
     return reg2.sub('', first_pass)
 
 def language_det(s):
+    """Detects the language of a string.
+
+    Args:
+        s (str): Input string.
+
+    Returns:
+        str: Language of the string; if the program cannot detect the language, it returns 'symbols'.
+    """
     if s == '':
         return 'empty'
     try:
@@ -55,11 +89,27 @@ def language_det(s):
         return 'symbols'
     
 def remove_stopwords(s):
+    """Remove stopwords from a string.
+
+    Args:
+        s (str): Input string.
+
+    Returns:
+        str: String without the stopwords.
+    """
     stop_words = set(stopwords.words('english'))
     tokens = word_tokenize(s)
     return ' '.join([w for w in tokens if w not in stop_words])
 
 def stemming(s):
+    """Stems a given string.
+
+    Args:
+        s (str): Input string.
+
+    Returns:
+        str: String stemmed.
+    """
     ps = PorterStemmer()
     tokens = word_tokenize(s)
     return ' '.join([ps.stem(w) for w in tokens])
@@ -69,6 +119,14 @@ def stemming(s):
 # Indexes
 
 def term_index(documents):
+    """Extract the vocabulary from a given corpus of documents.
+
+    Args:
+        documents (list): List of strings.
+
+    Returns:
+        dict: Dictionary containing as keys the words that appear in all the documents, as values an integer (each word is mapped with a unique integer).
+    """
     words = set()
     for s in documents:
         try:
@@ -83,6 +141,15 @@ def term_index(documents):
     return term_index
 
 def inverted_index(documents, term_indexes):
+    """Computes the inverted index for a corpus of documents.
+
+    Args:
+        documents (list): List of strings.
+        term_indexes (dict): Vocabulary dictionary containing the term-index maps.
+
+    Returns:
+        dict: Dictionary containing as keys the token ids found in the vocabulary, as values the documents in which each word appears.
+    """
     inv_index = defaultdict(list)
     for i, s in enumerate(documents):
         try:
@@ -95,6 +162,16 @@ def inverted_index(documents, term_indexes):
     return inv_index
 
 def tfidf_inv_indexes(documents, term_indexes, inv_indexes):
+    """Computes the inverted index with tfidf values for each document.
+
+    Args:
+        documents (list): List of strings.
+        term_indexes (dict): Vocabulary dictionary containing the term-index maps.
+        inv_indexes ([type]): Inverted index dictionary containing the index-documents maps.
+
+    Returns:
+        dict: Dictionary similar to the inverted index; each document also presents the tfidf value for that particular doc.
+    """
     tfidf_indexes = defaultdict(dict)
     for doc_id, s in enumerate(documents):
         try:
@@ -131,12 +208,32 @@ def tfidf_inv_indexes(documents, term_indexes, inv_indexes):
 ## Vanilla search engine
 
 class SimpleSearchEngine:
+    """A class representing a naive search engine, meaning that it will not perform any kind of sorting of the results.
+    
+    Attributes:
+        df (pd.DataFrame): Dataframe containingt the data on which the search engine is going to work.
+        term_indexes (dict): Dictionary containing the vocabulary with which the search engine is going to use to represent queries.
+        inv_indexes (dict): Dictionary containing the information with which the search engine is going to retrieve books on which the query appears.
+    
+    Methods:
+        search: Function which is going to be used just internally, used to retrieve the slice of the dataframe containing the words of the query.
+        execute_query: Displays the results of the conjunctive query in the proper way.
+    
+    """
     def __init__(self, df, term_indexes, inv_indexes):
         self.df = df
         self.term_indexes = term_indexes
         self.inv_indexes = inv_indexes
         
     def search(self, query):
+        """Returns slice of dataframe containing the query (use only internally).
+
+        Args:
+            query (str): String containing the query words the user wants to search for.
+
+        Returns:
+            pd.DataFrame: Resulting dataframe.
+        """
         # Since we performed stemming on the plot column of the dataframe, we need to
         # perform stemming also on the query. Otherwise, our results wouldn't be accurate
         query = query.lower()
@@ -163,23 +260,51 @@ class SimpleSearchEngine:
         return pd.DataFrame(data=self.df[self.df['index'].isin(documents_id)])
     
     def execute_query(self, query):
+        """Use the search function to retrieve the right documents, then return in the proper way.
+
+        Args:
+            query (str): String containing the query words the user wants to search for.
+
+        Returns:
+            pd.DataFrame: Dataframe of the conjunctive query with only 3 columns.
+        """
         return self.search(query)[['bookTitle', 'Plot', 'Url']]
 
 ## Scoring functions
 
 class RankCalculator():
+    """Class representing the type of a ranking calculator, that is a scoring function. Every rank calculator is going to have a value between 0 and 1."""
     def rank(self, book):
         pass
     
 class WeightedRanks(RankCalculator):
+    """Class containing the calculations of the weighted sum of a list of scoring functions and their respective weight.
+    This class is going to be used when defining a ranked search engine.
+    
+    Attributes:
+        calculators (list): List of tuples; first element of the tuple consists in the weight we want to give to a certain scoring function, second element represents the scoring function itself.
+    Methods:
+        rank: Computes the weighted sum of the ranking functions.
+    """
     def __init__(self, calculators):
         self.calculators = calculators
         
     def rank(self, book, query, token_ids):
+        """Computes the weighted similarity score for a certain document (book), a given query and the token ids of the query.
+
+        Args:
+            book (pd.DataFrame row): Row of the dataframe containing the info for a given book.
+            query (str): String containing the query that the user types.
+            token_ids (list): List containing the integers that we obtain mapping the query tokens with the vocabulary.
+
+        Returns:
+            float: Similarity score obtained with the weighted sum of the scoring functions for a certain book and a query. Notice that the returning value is in [0, 1].
+        """
         total_weight = np.sum([weight for weight, _ in self.calculators])
         return np.sum([calculator.rank(book, query, token_ids) * weight / total_weight for weight, calculator in self.calculators])
     
 class ByTfidf(RankCalculator):
+    """Scoring function using the cosin similarity over the tfidf values of a certain book and the query."""
     def __init__(self, tfidf_indexes):
         self.tfidf_indexes = tfidf_indexes
     
@@ -191,6 +316,7 @@ class ByTfidf(RankCalculator):
         return tfidf / np.sqrt(len(query.split()))
     
 class ByRatingValue(RankCalculator):
+    """Scoring function using the rating value of a certain book (score = book['ratingValue'] / max['ratingValue'])."""
     def __init__(self, df):
         self.max_rating = df['ratingValue'].max()
         
@@ -198,6 +324,7 @@ class ByRatingValue(RankCalculator):
         return book['ratingValue'] / self.max_rating
     
 class ByRatingCount(RankCalculator):
+    """Scoring function using the rating count of a certain book (score = book['ratingCount'] / max['ratingCount'])."""
     def __init__(self, df):
         self.max_rating = df['ratingCount'].max()
         
@@ -205,6 +332,7 @@ class ByRatingCount(RankCalculator):
         return book['ratingCount'] / self.max_rating
     
 class ByTitleMatch(RankCalculator):
+    """Scoring function using the exact match of query - book title (score = n_matches_title / len(title))."""
     def rank(self, book, query, token_ids):
         title_lenght = len(word_tokenize(book['bookTitle']))
         matches = 0
@@ -216,6 +344,20 @@ class ByTitleMatch(RankCalculator):
 ## Ranked search engine
 
 class RankedSearchEngine:
+    """A class representing a naive search engine, meaning that it will not perform any kind of sorting of the results.
+    
+    Attributes:
+        df (pd.DataFrame): Dataframe containingt the data on which the search engine is going to work.
+        term_indexes (dict): Dictionary containing the vocabulary with which the search engine is going to use to represent queries.
+        inv_indexes (dict): Dictionary containing the information with which the search engine is going to retrieve books on which the query appears.
+        simple_SE (class): Vanilla search engine, used to retrieve the results of the conjunctive query.
+        rank_calculator (class): Scoring function, used to sort the results of a query.
+        
+        
+    Methods:
+        execute_query: Displays the results of the conjunctive query, sorted according to the given rank calculator.
+    
+    """
     def __init__(self, df, term_indexes, inv_indexes, tfidf_indexes, simple_SE, rank_calculator):
         self.df = df
         self.term_indexes = term_indexes
@@ -225,6 +367,15 @@ class RankedSearchEngine:
         self.rank_calculator = rank_calculator
         
     def execute_query(self, query, k=10):
+        """Retrieve the conjunctive query results, sort them according to the similarity score and extract first k documents.
+
+        Args:
+            query (str): Query string given by the user.
+            k (int, optional): Number of documents to show with the query. Defaults to 10.
+
+        Returns:
+            pd.DataFrame: Dataframe containing the results of the query, with their similarity score.
+        """
         # First stem the query
         query = query.lower()
         ps = PorterStemmer()
@@ -259,6 +410,14 @@ class RankedSearchEngine:
 # Exercise 4
 
 def cumpage_serie(df):
+    """Helper function: shows and returns the cumulative pages distribution of the first ten book series in order of appearances in the years of publication.
+
+    Args:
+        df (pd.DataFrame): Input dataframe.
+
+    Returns:
+        pd.DataFrame: Dataframe containing data along the years for the series, representing when each book serie has published some book with number of pages.
+    """
     vis = df[df['bookSeries'].notnull()]
     vis = vis[vis['bookSeries'].str.contains('#')]
     vis = vis[~vis['bookSeries'].str.contains(r'#\d[-–]')]
@@ -302,6 +461,11 @@ def cumpage_serie(df):
     return vis
 
 def year_pages_visualization(series_pages):
+    """Plots the cumulative number of pages for the year-story of the first ten book series (for appearance).
+
+    Args:
+        series_pages (pd.DataFrame): Dataframe obtained with the cumpage_serie helper function.
+    """
     series = []
     for col in series_pages.columns:
         series.append(series_pages[series_pages[col].notna()][col].copy())
